39d8ee08ff8ac1358f95597c9bc524fac5bce1ca
kirtikabburi-svg <kirtikabburi@gmail.com>
2025-11-25 20:32:14 +0530

    Fix Render PORT issue

--- calmconnect.js
+++ calmconnect.js
@@ -154,8 +154,9 @@ app.get("/mood", (req, res) => {
   res.json({ moodLog });
 });
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () =>
   console.log(`CalmConnect server running on port ${PORT}`)
 );
+
 
